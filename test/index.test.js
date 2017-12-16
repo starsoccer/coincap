23d4d9e7eb3fe50c7ddd3ae9fd4ec2fab3ed48c0
starsoccer <1938666+starsoccer@users.noreply.github.com>
2017-12-15 21:27:33 -0500

    Added better tests checking for BTC and bumped version

--- test/index.test.js
+++ test/index.test.js
@@ -1,37 +1,77 @@
 const coincap = require('../src');
 
 describe('/coins', () => {
-
-    test('loads', () => {
-        coincap.getCoins().then(result => {
-            expect(result.result).toBeTruthy();
-            expect(result.statusCode).toBe(200);
-            expect(result.data.length).toBeGreaterThan(0);
+    let result;
+    beforeAll(() => {
+        return coincap.getCoins().then(data => {
+            result = data;
+            return data;
         });
     });
+      
+    test('loads', () => {
+        expect(result.result).toBeTruthy();
+        expect(result.statusCode).toBe(200);
+        expect(result.data.length).toBeGreaterThan(0);
+    });
 
+    test('check for BTC', () => {
+        expect(result.data.indexOf('BTC')).toBeGreaterThan(0);
+    });
 });
 
 describe('/map', () => {
-    
-    test('loads', () => {
-        coincap.map().then(result => {
-            expect(result.result).toBeTruthy();
-            expect(result.statusCode).toBe(200);
-            expect(result.data.length).toBeGreaterThan(0);
+    let result;
+    beforeAll(() => {
+        return coincap.map().then(data => {
+            result = data;
+            return data;
         });
+    });
+      
+    test('loads', () => {
+        expect(result.result).toBeTruthy();
+        expect(result.statusCode).toBe(200);
+        expect(result.data.length).toBeGreaterThan(0);
+    });
+
+    test('check for BTC', () => {
+        foundBTC = false;
+        for(coin of result.data) {
+            if (coin.symbol === 'BTC'){
+                foundBTC = true;
+                break;
+            }
+        }
+        expect(foundBTC).toBeTruthy();
     });
 
 });
 
 describe('/front', () => {
-    
-    test('loads', () => {
-        coincap.front().then(result => {
-            expect(result.result).toBeTruthy();
-            expect(result.statusCode).toBe(200);
-            expect(result.data.length).toBeGreaterThan(0);
+    let result;
+    beforeAll(() => {
+        return coincap.front().then(data => {
+            result = data;
+            return data;
         });
+    });
+      
+    test('loads', () => {
+        expect(result.result).toBeTruthy();
+        expect(result.statusCode).toBe(200);
+        expect(result.data.length).toBeGreaterThan(0);
+    });
+
+    test('check for BTC', () => {
+        foundBTC = false;
+        for(coin of result.data) {
+            if (coin.short === 'BTC'){
+                foundBTC = true;
+                break;
+            }
+        }
+        expect(foundBTC).toBeTruthy();
     });
 
 });
