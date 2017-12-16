58c1b7ddd3ddbcdd68ce5bcd4c49a6d88a9104a0
starsoccer <1938666+starsoccer@users.noreply.github.com>
2017-12-15 20:50:49 -0500

    Added /map call and test, and bumped version

--- src/index.js
+++ src/index.js
@@ -2,6 +2,17 @@ const url = 'http://coincap.io';
 const got = require('got');
 
 exports.getCoins = async function getCoins() {
-    const result = await got(`${url}/coins`);
-    return { result: result.statusCode === 200, statusCode: result.statusCode, data: result.body };
+    return createResult(await got(`${url}/coins`));
+}
+
+exports.map = async function map() {
+    return createResult(await got(`${url}/map`));
+}
+
+function createResult(data) {
+    const result = {result : data.statusCode === 200, statusCode: data.statusCode, data: data.body};
+    if (result.statusCode === 200) {
+        result.data = JSON.parse(result.data);
+    }
+    return result;
 }
--- test/index.test.js
+++ test/index.test.js
@@ -1,8 +1,21 @@
+const coincap = require('../src');
+
 describe('/coins', () => {
 
     test('loads', () => {
-        const coincap = require('../src');
         coincap.getCoins().then(result => {
+            expect(result.result).toBeTruthy();
+            expect(result.statusCode).toBe(200);
+            expect(JSON.parse(result.data).length).toBeGreaterThan(0);
+        });
+    });
+
+});
+
+describe('/map', () => {
+    
+    test('loads', () => {
+        coincap.map().then(result => {
             expect(result.result).toBeTruthy();
             expect(result.statusCode).toBe(200);
             expect(JSON.parse(result.data).length).toBeGreaterThan(0);
